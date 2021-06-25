dbbd2c6ce257af889143e2950865a443e13aaa2a
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2021-06-25 17:12:30 -0400

    changing names part 2

--- ad-hoc-sensei/src/pages/index.js
+++ ad-hoc-sensei/src/pages/index.js
@@ -1,3 +1,3 @@
-export {default as Home } from './Home'
-export {default as Login } from './Login'
+export {default as Home } from './Homex'
+export {default as Login } from './Loginx'
 export {default as Register } from './Register'
